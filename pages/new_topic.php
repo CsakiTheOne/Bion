1e1a12624e8b91df5f492b3bfcdf6144684cdf4c
Linux1230 <pekker77@gmail.com>
2020-03-30 18:08:41 +0200

    pages

--- pages/new_topic.php
+++ pages/new_topic.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Baloo+Thambi|Nova+Flat&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="topic.css">
+    <link rel="stylesheet" href="../css/topic.css">
   
 </head>
 <body>
