87f4aab536b854cee6a0a49f98954f5ebd4133ae
Linux1230 <pekker77@gmail.com>
2020-04-08 15:07:12 +0200

    asd

--- pages/about.php
+++ pages/about.php
@@ -22,7 +22,8 @@ include "../php/db/execute.php";
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <script src="https://kit.fontawesome.com/a866d5ef98.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="../css/style.css">
+  
+  <link rel="stylesheet" href="../css/about.css">
 </head>
 
 <body>
